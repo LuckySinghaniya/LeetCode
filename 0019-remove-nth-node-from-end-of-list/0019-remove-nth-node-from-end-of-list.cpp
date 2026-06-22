e47fe0d93c89a19ff703829e8587b6c8060d5bb4
LuckySinghaniya <luckysinghaniya117@gmail.com>
2026-06-23 01:19:50 +0530

    Time: 0 ms (100%), Space: 15.1 MB (24.95%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head==nullptr || head->next==nullptr) return nullptr;
+        ListNode* temp=head;
+        int count=0;
+        while(temp!=nullptr){
+            count++;
+            temp=temp->next;
+        }
+        count-=n;
+        if(count==0){
+            head=head->next;
+            return head;
+        }
+        temp=head;
+        while(count>1){
+            temp=temp->next;
+            count--;
+        }
+        temp->next=temp->next->next;
+        return head;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
