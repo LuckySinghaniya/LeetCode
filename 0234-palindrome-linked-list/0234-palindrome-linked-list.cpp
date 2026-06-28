eef2243deaa1f94558ea63ce041514bc5bafdb66
LuckySinghaniya <luckysinghaniya117@gmail.com>
2026-06-29 05:23:37 +0530

    Time: 0 ms (100%), Space: 127.5 MB (22.29%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int>s;
+        ListNode* temp=head;
+        while(temp!=nullptr){
+            s.push(temp->val);
+            temp=temp->next;
+        }
+        temp=head;
+        while(!s.empty()){
+            if(s.top()!=temp->val) return false;
+            else{
+                temp=temp->next;
+                s.pop();
+            }
+        }
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
