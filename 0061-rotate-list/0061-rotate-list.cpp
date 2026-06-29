857fc2382710cad76c93155277c4039e6c2113a8
LuckySinghaniya <luckysinghaniya117@gmail.com>
2026-06-30 05:10:47 +0530

    Time: 0 ms (100%), Space: 16.5 MB (31.51%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==nullptr || head->next==nullptr) return head;
+        ListNode* temp=head;
+        int len=1;
+        while(temp->next){
+            temp=temp->next;
+            len++;
+        }
+        k%=len;
+        temp->next=head;
+        int num=len-k;
+        while(num){
+            temp=temp->next;
+            num--;
+        }
+        head=temp->next;
+        temp->next=nullptr;
+        return head;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
