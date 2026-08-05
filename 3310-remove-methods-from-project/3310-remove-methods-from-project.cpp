f5e94350d41c2a19fbc78590337dc46558052157
LuckySinghaniya <luckysinghaniya117@gmail.com>
2026-08-05 09:20:16 +0530

    Time: 170 ms (74.14%), Space: 308.3 MB (60%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& adj,
+             vector<int>& vis) {
+
+        vis[node] = 1;
+
+        for (int nxt : adj[node]) {
+            if (!vis[nxt]) {
+                dfs(nxt, adj, vis);
+            }
+        }
+    }
+
+    vector<int> remainingMethods(int n, int k,
+                                 vector<vector<int>>& invocations) {
+
+        vector<vector<int>> adj(n);
+
+        for (auto& edge : invocations) {
+            adj[edge[0]].push_back(edge[1]);
+        }
+
+        vector<int> vis(n, 0);
+
+        dfs(k, adj, vis);
+
+        for (auto& edge : invocations) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (!vis[u] && vis[v]) {
+                vector<int> ans;
+
+                for (int i = 0; i < n; i++) {
+                    ans.push_back(i);
+                }
+
+                return ans;
+            }
+        }
+
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
